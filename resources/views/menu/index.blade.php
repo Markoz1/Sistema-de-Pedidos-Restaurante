0af8c2c46ca9776210cd00fe5813567968a6d0eb
markoz1 <marco.jhoel@gmail.com>
2018-05-05 23:56:42 -0400

    Diseño básico de la interfaz gráfica

--- resources/views/menu/index.blade.php
+++ resources/views/menu/index.blade.php
@@ -1,4 +1,193 @@
 @extends('layout.main-menu')
 @section('content')
-    contedido menu cliente
+    <article class="content">
+        <section class="section">
+            <div class="card-block">
+                <div class="row">
+                    <div class="col-xl-8">
+                        <div class="card-title-block">
+                            <h3 class="title"> Pill Tabs </h3>
+                        </div>
+                        <!-- Nav tabs -->
+                        <ul class="nav nav-pills mt-4">
+                            <li class="nav-item">
+                                <a href="" class="nav-link active" data-target="#home-pills" aria-controls="home-pills" data-toggle="tab" role="tab">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="" class="nav-link" data-target="#profile-pills" aria-controls="profile-pills" data-toggle="tab" role="tab">Profile</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="" class="nav-link" data-target="#messages-pills" aria-controls="messages-pills" data-toggle="tab" role="tab">Messages</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="" class="nav-link" data-target="#settings-pills" aria-controls="settings-pills" data-toggle="tab" role="tab">Settings</a>
+                            </li>
+                        </ul>
+                        <!-- Tab panes -->
+                        <div class="tab-content mt-4">
+                            <div class="tab-pane fade in active" id="home-pills">
+                                <div class="card-deck mt-4">
+                                    {{-- falta poner un efecto hover --}}
+                                    <div class="card border-primary mb-3" class="" data-toggle="modal" data-target="#exampleModal">
+                                        <img class="card-img-top" src="http://lorempixel.com/360/180/food/" alt="Card image cap">
+                                        <div class="card-body">
+                                            <h5 class="card-title">ejemplo para boton-card</h5>
+                                            <p class="card-text">34.12 BOB</p>
+                                        </div>
+                                    </div>
+                                    <div class="card border-primary mb-3">
+                                        <img class="card-img-top" src="http://lorempixel.com/360/180/food/" alt="Card image cap">
+                                        <div class="card-body">
+                                            <h5 class="card-title">Nombre Plato</h5>
+                                            <p class="card-text">34.12 BOB</p>
+                                        </div>
+                                    </div>
+                                    <div class="card border-primary mb-3">
+                                        <img class="card-img-top" src="http://lorempixel.com/360/180/food/" alt="Card image cap">
+                                        <div class="card-body">
+                                            <h5 class="card-title">Nombre Plato</h5>
+                                            <p class="card-text">34.12 BOB</p>
+                                        </div>
+                                    </div>
+                                    <div class="card border-primary mb-3">
+                                        <img class="card-img-top" src="http://lorempixel.com/360/180/food/" alt="Card image cap">
+                                        <div class="card-body">
+                                            <h5 class="card-title">Nombre Plato</h5>
+                                            <p class="card-text">34.12 BOB</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="card-deck">
+                                    <div class="card border-primary mb-3">
+                                        <img class="card-img-top" src="http://lorempixel.com/360/180/food/" alt="Card image cap">
+                                        <div class="card-body">
+                                            <h5 class="card-title">Nombre Plato</h5>
+                                            <p class="card-text">34.12 BOB</p>
+                                        </div>
+                                    </div>
+                                    <div class="card border-primary mb-3">
+                                        <img class="card-img-top" src="http://lorempixel.com/360/180/food/" alt="Card image cap">
+                                        <div class="card-body">
+                                            <h5 class="card-title">Nombre Plato</h5>
+                                            <p class="card-text">34.12 BOB</p>
+                                        </div>
+                                    </div>
+                                    <div class="card border-primary mb-3">
+                                        <img class="card-img-top" src="http://lorempixel.com/360/180/food/" alt="Card image cap">
+                                        <div class="card-body">
+                                            <h5 class="card-title">Nombre Plato</h5>
+                                            <p class="card-text">34.12 BOB</p>
+                                        </div>
+                                    </div>
+                                    <div class="card border-primary mb-3">
+                                        <img class="card-img-top" src="http://lorempixel.com/360/180/food/" alt="Card image cap">
+                                        <div class="card-body">
+                                            <h5 class="card-title">Nombre Plato</h5>
+                                            <p class="card-text">34.12 BOB</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="tab-pane fade" id="profile-pills">
+                                <div class="card-deck mt-4"> 
+                                    {{-- falta poner un efecto hover --}}
+                                    <div class="card border-primary mb-3"  class="" data-toggle="modal" data-target="#exampleModal">
+                                        <img class="card-img-top" src="http://lorempixel.com/360/180/food/" alt="Card image cap">
+                                        <div class="card-body">
+                                            <h5 class="card-title">ejemplo para boton-card</h5>
+                                            <p class="card-text">34.12 BOB</p>
+                                        </div>
+                                    </div>
+                                    <div class="card border-primary mb-3">
+                                        <img class="card-img-top" src="http://lorempixel.com/360/180/food/" alt="Card image cap">
+                                        <div class="card-body">
+                                            <h5 class="card-title">Nombre Plato</h5>
+                                            <p class="card-text">34.12 BOB</p>
+                                        </div>
+                                    </div>
+                                    <div class="card border-primary mb-3">
+                                        <img class="card-img-top" src="http://lorempixel.com/360/180/food/" alt="Card image cap">
+                                        <div class="card-body">
+                                            <h5 class="card-title">Nombre Plato</h5>
+                                            <p class="card-text">34.12 BOB</p>
+                                        </div>
+                                    </div>
+                                    <div class="card border-primary mb-3">
+                                        <img class="card-img-top" src="http://lorempixel.com/360/180/food/" alt="Card image cap">
+                                        <div class="card-body">
+                                            <h5 class="card-title">Nombre Plato</h5>
+                                            <p class="card-text">34.12 BOB</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="card-deck">
+                                    <div class="card border-primary mb-3">
+                                        <img class="card-img-top" src="http://lorempixel.com/360/180/food/" alt="Card image cap">
+                                        <div class="card-body">
+                                            <h5 class="card-title">Nombre Plato</h5>
+                                            <p class="card-text">34.12 BOB</p>
+                                        </div>
+                                    </div>
+                                    <div class="card border-primary mb-3">
+                                        <img class="card-img-top" src="http://lorempixel.com/360/180/food/" alt="Card image cap">
+                                        <div class="card-body">
+                                            <h5 class="card-title">Nombre Plato</h5>
+                                            <p class="card-text">34.12 BOB</p>
+                                        </div>
+                                    </div>
+                                    <div class="card border-primary mb-3">
+                                        <img class="card-img-top" src="http://lorempixel.com/360/180/food/" alt="Card image cap">
+                                        <div class="card-body">
+                                            <h5 class="card-title">Nombre Plato</h5>
+                                            <p class="card-text">34.12 BOB</p>
+                                        </div>
+                                    </div>
+                                    <div class="card border-primary mb-3">
+                                        <img class="card-img-top" src="http://lorempixel.com/360/180/food/" alt="Card image cap">
+                                        <div class="card-body">
+                                            <h5 class="card-title">Nombre Plato</h5>
+                                            <p class="card-text">34.12 BOB</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="tab-pane fade" id="messages-pills">
+                                contenido
+                            </div>
+                            <div class="tab-pane fade" id="settings-pills">
+                                contenido
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-4">
+                        @include('pedidos.create')
+                    </div>
+                </div>
+            </div>
+            <!-- /.card-block -->
+        </section>
+        <!-- Button trigger modal -->
+    
+    
+        <!-- Modal -->
+        <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                      <span aria-hidden="true">&times;</span>
+                    </button>
+                    </div>
+                    <div class="modal-body">
+                        ...
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary">Save changes</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </article>
 @endsection
---
+++ resources/views/pedidos/create.blade.php
@@ -0,0 +1,76 @@
+<div class="card card-primary">
+    <div class="card-header d-flex justify-content-center">
+        <div class="header-block">
+            <p class="title"> Mesa # </p>
+        </div>
+    </div>
+    <div class="card-block">
+        <div class="card-title-block py-2">
+            <h3 class="title"> Pedido Actual </h3>
+        </div>
+        <section class="example">
+            <table class="table table-hover">
+                <thead>
+                    <tr>
+                        <th>Producto</th>
+                        <th>Cant.</th>
+                        <th>Subtotal</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr height="85">
+                        <td class="align-middle">Pique</td>
+                        <td class="align-middle">2</td>
+                        <td class="align-middle text-center">40</td>
+                        <td class="align-middle">
+                            <a class="btn btn-oval btn-danger-outline btn-lg border-0" href="#" role="button">
+                                <i class="fa fa-trash-o"></i>
+                            </a>
+                        </td>
+                    </tr>
+                    <tr height="85">
+                        <td class="align-middle">Pique de algo</td>
+                        <td class="align-middle">2</td>
+                        <td class="align-middle text-center">40</td>
+                        <td class="align-middle">
+                            <a class="btn btn-oval btn-danger-outline btn-lg border-0" href="#" role="button">
+                                                    <i class="fa fa-trash-o"></i>
+                                                </a>
+                        </td>
+                    </tr>
+                    <tr height="85">
+                        <td class="align-middle">Pique de algo</td>
+                        <td class="align-middle">2</td>
+                        <td class="align-middle text-center">40</td>
+                        <td class="align-middle">
+                            <a class="btn btn-oval btn-danger-outline btn-lg border-0" href="#" role="button">
+                                                    <i class="fa fa-trash-o"></i>
+                                                </a>
+                        </td>
+                    </tr>
+                    <tr height="85">
+                        <td class="align-middle">Pique de algo</td>
+                        <td class="align-middle">2</td>
+                        <td class="align-middle text-center">40</td>
+                        <td class="align-middle">
+                            <a class="btn btn-oval btn-danger-outline btn-lg border-0" href="#" role="button">
+                                                    <i class="fa fa-trash-o"></i>
+                                                </a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colspan="2" class="text-center">TOTAL (Bs)</td>
+                        <td class="text-center">45</td>
+                        <td></td>
+                    </tr>
+                </tbody>
+            </table>
+        </section>
+    </div>
+    <div class="card-footer">
+        <div class="form-group row my-0 d-flex justify-content-around">
+            <button type="submit" class="btn btn-primary btn-lg"> Ordenar </button>
+        </div>
+    </div>
+</div>
