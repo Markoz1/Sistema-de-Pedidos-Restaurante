3ef6c9320e1cd9294a8dcca434ed15b40cf363b1
erickvelasco <erickvelasco2310@gmail.com>
2018-05-24 09:12:26 -0400

    crear modelo cliente

--- app/Model/Cliente.php
+++ app/Model/Cliente.php
@@ -6,10 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Cliente extends Model
 {
-    protected $table = 'cliente';
-    protected $primaryKey = 'cliente_id';
-
-    public $fillable = [
-        'cliente_id','nombre','nit', 'telefono', 'direccion'
-    ];
+   
 }
