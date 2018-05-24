0a6b3abda3a92ab365afed4ef73be2458407557b
erickvelasco <erickvelasco2310@gmail.com>
2018-05-24 09:14:16 -0400

    crear controlador clienteController

--- app/Model/Cliente.php
+++ app/Model/Cliente.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Cliente extends Model
 {
-   
+    protected $table = 'cliente';
+    protected $primaryKey = 'cliente_id';
+
+    public $fillable = [
+        'cliente_id','nombre','nit', 'telefono', 'direccion'
+    ];
 }
