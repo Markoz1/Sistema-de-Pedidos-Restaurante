cb9d2c3753bed6bbd2ae46266b20c7b1871cdc88
sergioJ92 <jeral.sergio@gmail.com>
2018-05-26 02:09:25 -0400

    vista vategorias index arreglado

--- resources/views/categorias/index.blade.php
+++ resources/views/categorias/index.blade.php
@@ -1,105 +1,119 @@
 @extends('layout.main')
 @section('content')
 
-<article class="content responsive-tables-page">
-    <div class="title-block">
-        <h1 class="title"> Categorias </h1>
-        <p class="title-description"> Lista de categorias </p>
-    </div>
-    @if(session('mensaje'))
-	    <div class="alert alert-dismissible alert-success">
-	        <button type="button" class="close" data-dismiss="alert">×</button>
-	        {{ session('mensaje') }}
-	    </div>
-    @endif
-    <section class="section">
-        <div class="card card-block sameheight-item">
-            <div class="title-block">
-                <h3 class="title"> Ingrese los datos de la nueva categoria </h3>
-            </div>
-            <form method="POST" role="form" action="{{url('categorias')}}" >
-                {{ csrf_field() }} 
-                <div class="form-group has-error">
-                    <label class="control-label">Nombre</label>
-                    <input type="text" class="form-control" name="nombreCategoria" value="{{ old('nombreCategoria')}}">
-                    @if( $errors->has('nombreCategoria') )
-                        <span class="has-error">
-                        {{ $errors->first('nombreCategoria') }}
-                        </span>							
-                    @endif             
-                </div>
-                <div class="form-group row mt-4">
-                    <div class="col-sm-10 col-sm-offset-2">
-                        <button type="submit" class="btn btn-primary"> Crear</button>
-     
-                    </div>
-                </div>
-            </form>
+    <article class="content items-list-page">
+        <div class="title-block">
+                <h1 class="title"> Categorias </h1>
+                <p class="title-description"> Lista de categorias </p>
         </div>
-    </section>
-    <section class="section">
-        <div class="row">
-            <div class="col-md-12">
-                <div class="card">
-                    <div class="card-block">
-                        <div class="card-title-block">
-                            <h3 class="title"> Tabla de categorias</h3>
+        @if(session('mensaje'))
+            <div class="alert alert-dismissible alert-success">
+                <button type="button" class="close" data-dismiss="alert">×</button>
+                {{ session('mensaje') }}
+            </div>
+        @endif
+        <section class="section">
+            <div class="card card-block sameheight-item">
+                <div class="title-block">
+                    <h3 class="title"> Ingrese los datos de la nueva categoria </h3>
+                </div>
+                <form method="POST" role="form" action="{{url('categorias')}}" >
+                    {{ csrf_field() }} 
+                    <div class="form-group has-error">
+                        <label class="control-label">Nombre</label>
+                        <input type="text" class="form-control" name="nombreCategoria" value="{{ old('nombreCategoria')}}">
+                        @if( $errors->has('nombreCategoria') )
+                            <span class="has-error">
+                            {{ $errors->first('nombreCategoria') }}
+                            </span>							
+                        @endif             
+                    </div>
+                    <div class="form-group row mt-4">
+                        <div class="col-sm-10 col-sm-offset-2">
+                            <button type="submit" class="btn btn-primary"> Crear</button>
+            
                         </div>
-                        <section class="example">
-                            <div class="table-responsive">
-                                <table class="table table-striped table-bordered table-hover">
-                                    <thead>
-                                        <tr>
-                                            <th># Id</th>
-                                            <th>Nombre de la caategoria</th>
-                                            <th>Acciones</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        @foreach($categorias as $categoria)
-                                            <tr>
-                                                <td>{{$categoria->categoria_id}}</td>
-                                                <td>{{$categoria->nombre}}</td>
-                                                <th>
-                                                    <a href="{{ route('categorias.edit',[$categoria->categoria_id]) }}"> Editar </a>
-                                                    <div class="item-col fixed item-col-actions-dropdown">
-                                                            <div class="item-actions-dropdown">
-                                                                <a class="item-actions-toggle-btn">
-                                                                    <span class="inactive">
-                                                                        <i class="fa fa-cog"></i>
-                                                                    </span>
-                                                                    <span class="active">
-                                                                        <i class="fa fa-chevron-circle-right"></i>
-                                                                    </span>
-                                                                </a>
-                                                                <div class="item-actions-block">
-                                                                    <ul class="item-actions-list">
-                                                                        <li>
-                                                                            <a class="remove" href="#" data-toggle="modal" data-target="#confirm-modal">
-                                                                                <i class="fa fa-trash-o "></i>
-                                                                            </a>
-                                                                        </li>
-                                                                        <li>
-                                                                            <a class="edit" href="#">
-                                                                                <i class="fa fa-pencil"></i>
-                                                                            </a>
-                                                                        </li>
-                                                                    </ul>
-                                                                </div>
-                                                            </div>
-                                                        </div>
-                                                </th>
-                                            </tr>
-                                        @endforeach
-                                    </tbody>
-                                </table>
-                            </div>
-                        </section>
                     </div>
-                </div>
+                </form>
             </div>
+        </section>
+        <div class="card items">
+            <ul class="item-list striped">
+                <li class="item item-list-header">
+                    <div class="item-row">
+                        <div class="item-col item-col-header fixed item-col-img md">
+                            <div>
+                                <span># Id</span>
+                            </div>
+                        </div>
+                        <div class="item-col item-col-header item-col-title">
+                            <div>
+                                <span>Nombre categoria</span>
+                            </div>
+                        </div>
+                        <div class="item-col item-col-header item-col-sales">
+                            <div>
+                                <span>Estado</span>
+                            </div>
+                        </div>
+                        <div class="item-col item-col-header item-col-date">
+                            <div>
+                                <span></span>
+                            </div>
+                        </div>
+                        <div class="item-col item-col-header fixed item-col-actions-dropdown"> </div>
+                    </div>
+                </li>
+                @foreach ($categorias as $categoria)
+                <li class="item">
+                    <div class="item-row">
+                        <div class="item-col fixed item-col-img md">
+                            <div class="item-heading"># Id</div>
+                            <div class="no-overflow"> {{ $categoria->categoria_id}} </div>
+                        </div>
+                        <div class="item-col fixed pull-left item-col-title">
+                            <div class="item-heading">Nombre</div>
+                            <div>
+                                <div class="no-overflow"> {{ $categoria->nombre }} </div>
+                            </div>
+                        </div>
+                        <div class="item-col item-col-sales">
+                            <div class="item-heading">Estado</div>
+                            <div class="no-overflow"> {{ '1' }}</div>
+                        </div>
+                        <div class="item-col item-col-date">
+                            <div class="item-heading">Acciones</div>
+                        </div>
+                        <div class="item-col fixed item-col-actions-dropdown">
+                            <div class="item-actions-dropdown">
+                                <a class="item-actions-toggle-btn">
+                                    <span class="inactive">
+                                        <i class="fa fa-cog"></i>
+                                    </span>
+                                    <span class="active">
+                                        <i class="fa fa-chevron-circle-right"></i>
+                                    </span>
+                                </a>
+                                <div class="item-actions-block">
+                                    <ul class="item-actions-list">
+                                        <li>
+                                            <a class="remove" href="#" data-toggle="modal" data-target="#confirm-modal">
+                                                <i class="fa fa-trash-o "></i>
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a class="edit" href="{{ route('categorias.edit',[$categoria->categoria_id]) }}">
+                                                <i class="fa fa-pencil"></i>
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+                @endforeach
+            </ul>
         </div>
-    </section>
-    
-</article>
+    </article>
 @endsection
