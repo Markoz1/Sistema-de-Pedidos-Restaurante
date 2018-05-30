83786728661212299ac977247a060d00ff9c0463
erickvelasco <erickvelasco2310@gmail.com>
2018-05-29 22:34:20 -0400

    actualizar sidebar

--- database/seeds/ClienteSeeder.php
+++ database/seeds/ClienteSeeder.php
@@ -11,6 +11,24 @@ class ClienteSeeder extends Seeder
      */
     public function run()
     {
-        Factory(Cliente::class,100)->create();       
+            Cliente::create([
+            'nombre'=> "pedro camacho",            
+            'nit'=> "123221222",
+            'telefono'=> "73788767",
+            'direccion'=> "av america #343"
+           ]);
+            Cliente::create([
+            'nombre'=> "juan perez",            
+            'nit'=> "8977789",
+            'telefono'=> "44345678",
+            'direccion'=> "av blanco galindo km 3"
+           ]);
+            Cliente::create([
+            'nombre'=> "adrian espinoza",            
+            'nit'=> "333333",
+            'telefono'=> "44265647",
+            'direccion'=> "av blanco galindo km 10"
+           ]);     
+        Factory(Cliente::class,15)->create();       
     }
 }
