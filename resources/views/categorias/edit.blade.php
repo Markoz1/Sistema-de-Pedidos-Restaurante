4d9af28b4ccf1e2e05b7c8c47bf89a79894bbeff
sergioJ92 <jeral.sergio@gmail.com>
2018-06-22 12:45:32 -0400

    template con formulario para reutilizar codigo

--- resources/views/categorias/edit.blade.php
+++ resources/views/categorias/edit.blade.php
@@ -2,36 +2,32 @@
 @section('content')
 <article class="content">
 	<section class="section">
-		<div class="card card-block sameheight-item" style="height: 733px;">
-            <div class="title-block">
-                <h3 class="title"> Editar categoria categoria </h3>
-            </div>
-            <form method="POST" role="form" action="{{ url("categorias/{$categoria->categoria_id}") }}" >
-                {{ method_field('PUT') }}
-            	{{ csrf_field() }} 
-                <div class="form-group has-error">
-                    <label class="control-label">Nombre Categoria</label>
-                    <input type="text" class="form-control" name="nombreCategoria" value="{{ old('nombreCategoria', $categoria->nombre)}}">
-                    @if( $errors->has('nombreCategoria') )
-                        <span class="has-error">
-                        {{ $errors->first('nombreCategoria') }}
-                        </span>							
-                    @endif 
+		<div class="card card-block sameheight-item">
+            <div class="container">
+                <div class="title-block">
+                    <h3 class="title"> Editar categoria categoria </h3>
                 </div>
-                <div class="form-group">
-                    <label class="control-label">Seleccione el estado de la categoria</label>
-                    <select class="form-control boxed" id="estado" name="estado" >
-                        <option value="1">Categoria activada</option>
-                        <option value="0">Categoria inactiva</option>
-                    </select>
-                </div>
-                <div class="form-group row mt-4">
-                    <div class="col-sm-10 col-sm-offset-2">
-                        <button type="submit" class="btn btn-primary">Actualizar</button>
-                        <a type="button" class="btn btn-primary" href="{{ route('categorias.index') }}">Regresar</a>
+                <form method="POST" role="form" action="{{ url("categorias/{$categoria->categoria_id}") }}" >
+                    {{ method_field('PUT') }}
+                    {{ csrf_field() }} 
+                    <div class="form-group has-error">
+                        <label class="control-label">Nombre Categoria</label>
+                        <input type="text" class="form-control" name="nombreCategoria" value="{{ old('nombreCategoria', $categoria->nombre)}}">
+                        @if( $errors->has('nombreCategoria') )
+                            <span class="has-error">
+                            {{ $errors->first('nombreCategoria') }}
+                            </span>							
+                        @endif 
                     </div>
-                </div>
-            </form>
+                    @include('categorias.formulario')
+                    <div class="form-group row mt-4">
+                        <div class="col-sm-10 col-sm-offset-2">
+                            <button type="submit" class="btn btn-primary">Actualizar</button>
+                            <a type="button" class="btn btn-primary" href="{{ route('categorias.index') }}">Regresar</a>
+                        </div>
+                    </div>
+                </form>
+            </div>
         </div>
 	</section>
   </article>
--- resources/views/mesas/admin/create.blade.php
+++ resources/views/mesas/admin/create.blade.php
@@ -25,18 +25,6 @@
                 </div>
             </div>
         </div>
-        <div class="form-group row">
-            <label for="estado" class="col-md-4 form-control-label text-xs-right">Estado</label>
-            <div class="col-md-8">
-                <label>
-                    <input class="radio squared estado1" checked="checked" name="estado" type="radio" value="1" id="estado">
-                    <span>Activo</span>
-                </label>
-                <label>
-                    <input class="radio squared estado0" name="estado" type="radio" value="0" id="estado">
-                    <span>Inactivo</span>
-                </label>
-            </div>
-        </div>
+        
     </section>
 </div>
