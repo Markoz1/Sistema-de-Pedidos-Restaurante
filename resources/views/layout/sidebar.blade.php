bce6f6c2ea4f6888938a1c06cc090b16be74411c
sergioJ92 <jeral.sergio@gmail.com>
2018-06-25 00:44:05 -0400

    cambiar el formato de categorias en  la barra de navegacion

--- resources/views/layout/sidebar.blade.php
+++ resources/views/layout/sidebar.blade.php
@@ -12,15 +12,7 @@
                         <i class="fa fa-home"></i> Inicio </a>
                 </li>
                 <li class="{{ request()->segment(1) == 'categorias'? 'active open':'' }}">
-                    <a href="">
-                        <i class="fa fa-cutlery"></i> Categorias
-                        <i class="fa arrow"></i> 
-                    </a>
-                    <ul class="sidebar-nav">
-                        <li class="{{ request()->routeIs('categorias.index')? 'active':'' }}">
-                            <a href="{{ route('categorias.index') }}"> Listado </a>
-                        </li>
-                    </ul>
+                    <a href="{{ route('categorias.index') }}"><i class="fa fa-cutlery"></i> Categorias</a>
                 </li>
                 <li class="{{ request()->segment(1) == 'productos'? 'active open':'' }}">
                     <a href="">
