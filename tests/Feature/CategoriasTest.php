bf17ea64afcccfcb205b6a72351dad0f4555307c
sergioJ92 <jeral.sergio@gmail.com>
2018-06-28 18:59:19 -0400

    test crear usurio completado

--- tests/Feature/CategoriasTest.php
+++ tests/Feature/CategoriasTest.php
@@ -5,9 +5,13 @@ namespace Tests\Feature;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Model\User;
+
 
 class CategoriasTest extends TestCase
 {
+    //use RefreshDatabase;
+
     /**
      * A basic test example.
      *
@@ -15,9 +19,33 @@ class CategoriasTest extends TestCase
      */
     public function acceder_a_categorias_como_administrador()
     {
-        $response = $this->withSession(['User' => 'admin'])
+        $user = User::find(1);
+        $response = $this->actingAs($user)
+            ->withSession(['User' => 'admin'])
              ->get('/categorias')
              ->assertStatus(200)
              ->assertSee('Categorias');          
     }
+    /**
+     * @test
+     */
+    public function crear_nueva_categoria(){
+        
+        $this->withoutExceptionHandling();
+
+        $user = User::find(1);
+        $response = $this->actingAs($user)
+            ->withSession(['User' => 'admin'])
+                ->json('POST', '/categorias', [
+                    'nombreCategoria' => 'ensalada',
+                    'estado' => true
+                    ])->assertRedirect('/categorias');
+            
+            $this->assertDatabaseHas('categoria',[
+                'nombre' => 'ensalada',
+                'estado' => true
+            ]);
+    }
+
+
 }
